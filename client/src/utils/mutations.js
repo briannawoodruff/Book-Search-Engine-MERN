0899b431ea81ff102e9c918502c5cfdeb9e28ff3
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-03-06 18:38:28 -0600

    client mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,60 @@
 import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: savedBook) {
+    saveBook(input: $input) {
+        _id
+        username
+        bookCount
+        savedBooks {
+            authors
+            description
+            bookId
+            image
+            link
+            title
+    }
+  }
+`;
+
+export const DELETE_BOOK = gql`
+  mutation deleteBook($bookId: String!) {
+    deleteBook(bookId: $bookId) {
+        _id
+        username
+        bookCount
+        savedBooks {
+            authors
+            description
+            bookId
+            image
+            link
+            title
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
