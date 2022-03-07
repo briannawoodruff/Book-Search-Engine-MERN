d8ccaa698a2a626391a501ac837a1c7cfc501432
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-03-06 18:27:26 -0600

    client queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,20 @@
 import { gql } from '@apollo/client';
+
+// from typeDefs
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+          authors
+          description
+          bookId
+          image
+          link
+          title
+      }
+    }
+  }
+`;
